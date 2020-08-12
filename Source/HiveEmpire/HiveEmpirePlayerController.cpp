3e8b39d35b60e7aaed81264b8a94efcdcb862564
Major Imre <imewster@gmail.com>
2020-08-12 12:00:00 +0200

    Hide mouse cursor

---
+++ Source/HiveEmpire/HiveEmpirePlayerController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HiveEmpirePlayerController.h"
+
+AHiveEmpirePlayerController::AHiveEmpirePlayerController(const FObjectInitializer& ObjectInitializer) :
+	Super(ObjectInitializer)
+{
+	bShowMouseCursor = true;
+	bEnableClickEvents = true;
+	bEnableMouseOverEvents = true;
+};
---
+++ Source/HiveEmpire/HiveEmpirePlayerController.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "HiveEmpirePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HIVEEMPIRE_API AHiveEmpirePlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+		AHiveEmpirePlayerController(const FObjectInitializer& ObjectInitializer);
+};
